030564d11392b5e25495352ba0e0c234677b548c
Jorge Rodríguez-Araújo <grrodri@gmail.com>
2017-11-13 17:47:46 +0100

    Add support for command line optimization of multiple files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='abraia',
-      version='0.1.3',
+      version='0.1.4',
       description='Abraia Python SDK',
       url='https://github.com/abraia/abraia-python',
       author='Jorge Rodriguez Araujo',
