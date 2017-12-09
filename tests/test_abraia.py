7806dbb7348cf592dd86127f73e8e5ccb5dcc32f
Jorge Rodríguez-Araújo <grrodri@gmail.com>
2017-12-09 17:40:40 +0100

    Fix test issues with remote images

--- tests/test_abraia.py
+++ tests/test_abraia.py
@@ -12,7 +12,7 @@ def test_from_file():
 
 def test_from_url():
     """Test an API call to upload a remote file"""
-    url = 'https://abraia.me/images/tiger.jpg'
+    url = 'https://abraia.me/images/random.jpg'
     source = abraia.from_url(url)
     assert isinstance(source, abraia.Client)
     assert source.params['url'] == url
@@ -22,7 +22,7 @@ def test_to_file():
     """Test an API call to save to local file"""
     output = os.path.join(
         os.path.dirname(__file__), '../images/optimized.jpg')
-    source = abraia.from_url('https://abraia.me/images/lion.jpg')
+    source = abraia.from_url('https://abraia.me/images/random.jpg')
     source.to_file(output)
     assert os.path.isfile(output)
 
@@ -31,7 +31,8 @@ def test_resize():
     """Test an API call to resize an image"""
     output = os.path.join(
         os.path.dirname(__file__), '../images/resized.jpg')
-    source = abraia.from_url('https://abraia.me/images/lion.jpg')
+    source = abraia.from_file(os.path.join(
+        os.path.dirname(__file__), '../images/lion.jpg'))
     resized = source.resize(width=150, height=150)
     resized.to_file(output)
     assert os.path.isfile(output)
@@ -39,7 +40,7 @@ def test_resize():
 
 def test_exception():
     """Test an API exception to save no file"""
-    source = abraia.from_url('https://abraia.me/images/tiger.jp')
+    source = abraia.from_url('https://abraia.me/images/tiger.jpg')
     with pytest.raises(abraia.APIError):
         source.to_file(os.path.join(
             os.path.dirname(__file__), '../images/error.jpg'))
