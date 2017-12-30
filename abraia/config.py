a4c1708d62dbdd768dce6142925fe692f4dc7279
Jorge Rodríguez-Araújo <grrodri@gmail.com>
2017-12-30 18:24:20 +0100

    Remove yaml dependency

--- abraia/config.py
+++ abraia/config.py
@@ -1,5 +1,4 @@
 import os
-import yaml
 
 api_url = 'https://abraia.me/api'
 config_file = os.path.join(os.path.expanduser('~'), '.abraia')
@@ -9,13 +8,16 @@ def load_auth():
     api_key = os.environ.get('ABRAIA_API_KEY')
     api_secret = os.environ.get('ABRAIA_API_SECRET')
     config = {'abraia_api_key': api_key, 'abraia_api_secret': api_secret}
-    if os.path.isfile(config_file) and api_key is None or api_secret is None:
+    if os.path.isfile(config_file) and (api_key is None or api_secret is None):
         with open(config_file, 'r') as f:
-            config = yaml.load(f)
+            for line in f:
+                key, value = list(map(lambda v: v.strip(), line.split(':')))
+                config[key] = value
     return config['abraia_api_key'], config['abraia_api_secret']
 
 
 def save_auth(api_key, api_secret):
-    config = {'abraia_api_key': api_key, 'abraia_api_secret': api_secret}
+    content = ('abraia_api_key: {}\n'
+               'abraia_api_secret: {}\n').format(api_key, api_secret)
     with open(config_file, 'w') as f:
-        yaml.dump(config, f, default_flow_style=False)
+        f.write(content)
