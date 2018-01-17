acef0aaa19a30e770eaf2e500801e84753d82891
Jorge Rodríguez-Araújo <grrodri@gmail.com>
2018-01-17 19:18:28 +0100

    Add recursive folder optimization

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='abraia',
-      version='0.2.0',
+      version='0.2.1',
       description='Abraia Python SDK',
       url='https://github.com/abraia/abraia-python',
       author='Jorge Rodriguez Araujo',
