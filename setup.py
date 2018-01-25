4c776a2a0cb83cecde0b02d8ff851d89f8f414c3
Jorge Rodríguez-Araújo <grrodri@gmail.com>
2018-01-25 10:15:51 +0100

    Add support for python 2.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='abraia',
-      version='0.2.5',
+      version='0.2.7',
       description='Abraia Python SDK',
       url='https://github.com/abraia/abraia-python',
       author='Jorge Rodriguez Araujo',
@@ -12,4 +12,4 @@ setup(name='abraia',
       tests_require=['pytest'],
       setup_requires=['pytest-runner'],
       scripts=['scripts/abraia', 'scripts/abraia.bat'],
-      install_requires=['requests', 'tqdm'])
+      install_requires=['requests', 'tqdm', 'future'])
